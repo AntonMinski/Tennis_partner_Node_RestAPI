371845f0b7fa954fbdd57da18d54827e82698bbf
Anton Minski <anton_minski5@ukr.net>
2021-08-27 10:21:03 +0300

    02_2 error middleware + async middleware

--- controllers/courts.js
+++ controllers/courts.js
@@ -1,25 +1,20 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Courts = require('../models/Courts');
 
 
 // desc: get all courts
 // route: GET /api/v1/courts
 // access: Public
-exports.getCourts = (req, res, next) => {
-    Courts.find()
-        .then(data =>
-            res.status(200).json({
-                success: true,
-                length: data.length,
-                data: data
-        })
-        )
-        .catch(err => {
-            next(new ErrorResponse(
-                `Court with id ${req.params.id} was not found`, 404));
-            }
-        )
-};
+exports.getCourts = asyncHandler(async (req, res, next) => {
+    const courts = await Courts.find();
+
+    res.status(200).json({
+            success: true,
+            length: courts.length,
+            data: courts
+        });
+});
 
 // desc: get one offer
 // route: GET /api/v1/courts/:id
@@ -31,31 +26,20 @@ exports.getCourt = (req, res, next) => {
                 success: true,
                 data: data
             }))
-        .catch(error => {
-            next(new ErrorResponse(
-                `Court with id <${req.params.id}> was not found`,
-                404));
-            });
+        .catch(err => next(err) );
 };
 
 // desc: create court
 // route: POST /api/v1/offers/
 // access: Private
-exports.postCourt = async (req, res, next) => {
-    console.log(req.body);
-    try {
+exports.postCourt = asyncHandler(async (req, res, next) => {
+
         const court = await Courts.create(req.body);
         res.status(201).json({
             sucess: true,
             data: court
         });
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            error: err
-        });
-    }
-};
+});
 
 // desc: edit offer
 // route: PUT /api/v1/offers/:id
@@ -70,12 +54,7 @@ exports.editCourt = (req, res, next) => {
                 success: true,
                 data: data
             }))
-        .catch(err => {
-            res.status(400).json({
-                success: false,
-                error: err
-            })
-        });
+        .catch(err => next(err));
 };
 
 // desc: delete offer
@@ -88,10 +67,5 @@ exports.deleteCourt = (req, res, next) => {
                 success: true,
                 data: {}
             }))
-        .catch(err => {
-            res.status(400).json({
-                success: false,
-                error: err
-            })
-        });
+        .catch(err => next(err));
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,31 @@
-const errorHandler = (error, req, res, next) => {
-    console.log(error.stack.red)
+const ErrorResponse = require('../utils/errorResponse')
+
+const errorHandler = (err, req, res, next) => {
+    let error = { ...err }
+    error.message = err.message
+
+
+    // console handling
+    console.log(error.message.red.bold)
+    // console.log(err.stack.red)
+
+    // Mongoose bad object
+    if (err.name === 'CastError') {
+        const message = `Object with id <${err.value}> was not found`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000 ) {
+        const message = 'Duplicate field entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation error
+    if (err.name === 'ValidationError' ) {
+        const message = Object.values(err.errors).map(value => value.message);
+        error = new ErrorResponse(message, 400);
+    }
 
     res.status(error.statusCode || 500).json({
         success: false,
